b069ebf69b5fb5a4fb14f0517f45ef6c40ff17b0
Rabah Abdul Khalek <rabahabdulkhalek@Rabahs-MacBook-Pro-2.local>
2024-03-29 14:06:08 +0100

    testing new report layout with 5 images

--- download_300W.sh
+++ download_300W.sh
@@ -1,5 +1,5 @@
 mkdir 300W
 cd 300W
-for i in $(seq -f "%03g" 1 300); do wget https://poc-face-aligment.s3.eu-north-1.amazonaws.com/300W/01_Indoor/indoor_$i.png; done
-for i in $(seq -f "%03g" 1 300); do wget https://poc-face-aligment.s3.eu-north-1.amazonaws.com/300W/01_Indoor/indoor_$i.pts; done
+for i in $(seq -f "%03g" 1 5); do wget https://poc-face-aligment.s3.eu-north-1.amazonaws.com/300W/01_Indoor/indoor_$i.png; done
+for i in $(seq -f "%03g" 1 5); do wget https://poc-face-aligment.s3.eu-north-1.amazonaws.com/300W/01_Indoor/indoor_$i.pts; done
 cd ..
